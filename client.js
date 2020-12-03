f758d14da180c6554972dd06e84aa18c9499dd8b
Eric4106 <flappy4106@hotmail.com>
2020-12-03 13:50:22 -0500

    Added and displayed data

--- client.js
+++ client.js
@@ -1,1 +1,84 @@
-document.querySelector("p").style.color = "red"
+console.log(loadData())
+
+const $moviesList = document.getElementById("moviesList");
+
+const renderReview = review => `<li>${review.rating}</li>`
+
+const renderMovie = movie => 
+`<div>
+    <br>
+    <h2>${movie.name}</h2>
+    <ul>${movie.reviews.map(renderReview).join('')}</ul>
+</div>`
+
+renderMovies();
+
+function renderMovies() {
+    $moviesList.innerHTML = loadData().movies.map(renderMovie).join('');
+}
+
+function loadData() {
+    //JSON - JavaScript Object Notation
+    const movies = [
+    {
+        "name" : "Dr. Strangelove",
+        "genre" : "Drama",
+        "reviews" : [
+            {
+                "rating" : 5,
+                "like" : true,
+                "user" : {
+                    "name" : "Eric Thomas",
+                    "gender" : "male",
+                    "region" : "USA"
+                }
+            },
+            {
+                "rating" : 5,
+                "like" : true,
+                "user" : {
+                    "name" : "Martin Thomas",
+                    "gender" : "male",
+                    "region" : "USA"
+                }
+            },
+            {
+                "rating" : 5,
+            },
+            {
+                "rating" : 4,
+            },
+            {
+                "rating" : 5,
+            },
+            {
+                "rating" : 3,
+            }
+        ]
+    },
+    {
+        "name" : "The Shawshank Redemption",
+        "genre" : "Drama",
+        "reviews" : [
+            {
+                "rating" : 5,
+                "like" : true,
+                "user" : {
+                    "name" : "Eric Thomas",
+                    "gender" : "male",
+                    "region" : "USA"
+                }
+            },
+            {
+                "rating" : 3,
+            },
+            {
+                "rating" : 5,
+            }
+        ]
+    }
+    ]
+    return{
+        "movies" : movies
+    }
+}
